d1998e1852faa3d64008332296974cde2c2d2183
Amir Muhammad <ameermohamed2500@gmail.com>
2021-04-05 01:07:26 +0200

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,115 @@
+import React from 'react';
 import './App.css';
+import 'weather-icons/css/weather-icons.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Weather from './components/WeatherComponent';
+import Form from './components/formComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <Weather />
-    </div>
-  );
+// http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid
+const API_KEY = 'df8e8d73a147acb9f4ff9e4b054f8cd5';
+
+
+class App extends React.Component {
+  
+  constructor() {
+     super();
+     this.state = {
+       city:undefined,
+       country:undefined,
+       icon:undefined,
+       main:undefined,
+       celsius:undefined,
+       temp_max:undefined,
+       temp_min:undefined,
+       description:'',
+       error:false
+     };
+
+     this.weatherIcon = {
+       Thunderstorm: 'wi-thunderstorm',
+       Drizzle:'wi-sleet',
+       Rain:'wi-storm-showers',
+       Snow:'wi-snow',
+       Atmosphere:'wi-fog',
+       Clear:'wi-day-sunny',
+       Clouds:'wi-day-fog'
+     }
+  }
+
+  calCel(temp) {
+    let cel = Math.floor(temp - 273.15);
+    return cel;
+  }
+
+  getWeatherIcon(icons,rangeId) {
+    switch(true) {
+      case rangeId >= 200 && rangeId <= 232:
+        this.setState({icon:this.weatherIcon.Thunderstorm});
+        break;
+      case rangeId >= 300 && rangeId <= 321:
+        this.setState({icon:this.weatherIcon.Drizzle});
+        break;
+      case rangeId >= 500 && rangeId <= 531:
+        this.setState({icon:this.weatherIcon.Rain});
+        break;
+      case rangeId >= 600 && rangeId <= 622:
+        this.setState({icon:this.weatherIcon.Snow});
+        break;
+      case rangeId >= 700 && rangeId <= 781:
+        this.setState({icon:this.weatherIcon.Atmosphere});
+        break;
+      case rangeId === 800:
+        this.setState({icon:this.weatherIcon.Clear});
+        break;
+      case rangeId >= 801 && rangeId <= 804:
+        this.setState({icon:this.weatherIcon.Clouds});
+        break;
+      default:
+        this.setState({icon:this.weatherIcon.Clouds});                         
+    }
+  }
+
+  getWeather = async (e) => {
+    e.preventDefault();
+
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+
+    if(city && country) {
+      
+      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+      const response = await api_call.json();
+
+      console.log(response);
+
+      this.setState({
+        city:`${response.name}, ${response.sys.country}`,
+        celsius:this.calCel(response.main.temp),
+        temp_min:this.calCel(response.main.temp_min),
+        temp_max:this.calCel(response.main.temp_max),
+        description:response.weather[0].description
+      });
+
+      this.getWeatherIcon(this.weatherIcon,response.weather[0].id);
+    } else {
+      this.setState({error:true})
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Form loadWeather={this.getWeather} error={this.state.error} />
+        <Weather 
+        city = {this.state.city} 
+        temp_celsius = {this.state.celsius} 
+        temp_max = {this.state.temp_max}
+        temp_min = {this.state.temp_min}
+        description = {this.state.description}
+        weatherIcon = {this.state.icon} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/formComponent.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './form.style.css';
+
+function error() {
+    return (
+        <div className="alert alert-danger mx-5" role="alert">
+            Please fill the above inputs
+        </div>
+    );
+}
+
+const form = props => {
+    console.log(props.error)
+    return (
+        <div className="container">
+            {props.error ? error() : null}
+            <form onSubmit={props.loadWeather}>
+                <div className="row">
+                    <div className="col-md-3 offset-md-2">
+                        <input type="text" className="form-control" placeholder="City" name="city" autoComplete="off"/>
+                    </div>
+                    <div className="col-md-3">
+                    <input type="text" className="form-control" placeholder="Country" name="country" autoComplete="off"/>
+                    </div>
+                    <div className="col-md-3 mt-md-0 py-3 text-md-left">
+                        <button className="btn btn-danger">Get Weather</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+
+export default form;
